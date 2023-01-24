1caead40bc0c2bbd5d7e5e6ff4ff9e69b793d392
Corven <fixer.slice@gmail.com>
2023-01-24 19:46:39 +0100

    Added libraries for minimap icon + graph (this one not used yet), created icons, addon no longer in the addon_interface list, added a toggle button but needs a better way to organize

--- core/globalMethods.lua
+++ core/globalMethods.lua
@@ -1,5 +1,6 @@
 GlobalMethods = {
-    test = nil
+    test = nil,
+	allowedOperators = {"+", "-", "*", "/", "%", "^"}
 }
 
 function GlobalMethods:timeChecker(sdate)
@@ -11,3 +12,41 @@ function GlobalMethods:timeChecker(sdate)
         day = d
     })
 end
+
+function GlobalMethods:tableContains(tab, val)
+    for k, v in pairs(tab) do
+        if v == val then 
+            return true
+        end
+    end
+    return false
+end
+
+function GlobalMethods:case(tester, callback)
+	if tester then
+		callback()
+		return true
+	end
+	return false
+end
+--[[
+	local i = 1
+	
+	for k = 1, 1 do
+		if case(i == 1, 
+			function () i = 2; print(i) end)
+		then break end
+		if case(i >= 2, 
+			function () i = 3; print(i) end)
+		then end
+		if case(i == 3,
+			function () print("test") end)
+		then break end
+		if case("default", 
+			function () print("default type") end)
+		then break end
+	end
+	
+	print("fin")
+]]--
+		
